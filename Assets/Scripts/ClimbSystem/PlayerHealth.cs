a2a3426c8aa689e0a8c98379abec172efaa3d2b9
김강현 <rlarkdgudjs@gmail.com>
2026-03-21 14:45:13 +0900

    PackMan
    유령수정

--- Assets/Scripts/ClimbSystem/PlayerHealth.cs
+++ Assets/Scripts/ClimbSystem/PlayerHealth.cs
@@ -1,50 +1,26 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.Animations.Rigging;
 
 public class PlayerHealth : MonoBehaviour
 {
-    public int maxHealth = 3;
-    public PlayerController climbingRig;
-    public GameObject[] hearts = new GameObject[3];
+    public PlayerController playerController;
 
-    [Header("ÇÏÆ® ±ôºýÀÓ")]
-    public float heartBlinkDuration = 0.8f;
-    public float heartBlinkInterval = 0.1f;
-
-    private int currentHealth;
 
     void Start()
     {
-        currentHealth = maxHealth;
     }
 
-    public void TakeDamage(float stunTime)
+    public void TakeStun(float stunTime)
     {
-        if (currentHealth <= 0) return;
-
-        currentHealth--;
-        Debug.Log($"ÇÇ°Ý! ³²Àº Ã¼·Â: {currentHealth}/{maxHealth}");
-
-        if (climbingRig != null)
-            climbingRig.Stun(stunTime);
-
-        // ÀÒÀº ÇÏÆ® ±ôºýÀÓ ÈÄ ²ô±â
-        if (currentHealth < hearts.Length && hearts[currentHealth] != null)
-            StartCoroutine(BlinkThenDisable(hearts[currentHealth]));
-
-        if (currentHealth <= 0)
-            Debug.Log("»ç¸Á!");
+        if (playerController != null)
+            playerController.Stun(stunTime);
     }
 
-    IEnumerator BlinkThenDisable(GameObject heart)
+    public void TakeSlide(float slideTime)
     {
-        float elapsed = 0f;
-        while (elapsed < heartBlinkDuration)
-        {
-            heart.SetActive(!heart.activeSelf);
-            yield return new WaitForSeconds(heartBlinkInterval);
-            elapsed += heartBlinkInterval;
-        }
-        heart.SetActive(false);
+        if(playerController != null)
+            playerController.Slide(slideTime);
     }
+
 }
