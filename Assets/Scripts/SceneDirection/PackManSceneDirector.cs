73b2aeaa13d05bb134ed1a782beb307151535145
김강현 <rlarkdgudjs@gmail.com>
2026-03-21 21:54:08 +0900

    ver14

--- Assets/Scripts/SceneDirection/PackManSceneDirector.cs
+++ Assets/Scripts/SceneDirection/PackManSceneDirector.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class PackManSceneDirector : MonoBehaviour
 {
+    public PlayerController controller;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -13,4 +14,6 @@ public class PackManSceneDirector : MonoBehaviour
     {
         
     }
+
+
 }
