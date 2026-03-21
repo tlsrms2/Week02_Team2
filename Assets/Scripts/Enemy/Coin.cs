574e6527b0c70e6cf3e3ca4a73e1554b400e70db
tlsrms2 <tiffha090@gmail.com>
2026-03-21 11:18:57 +0900

    코인 먹을 때 이펙트 추가

--- Assets/Scripts/Enemy/Coin.cs
+++ Assets/Scripts/Enemy/Coin.cs
@@ -2,13 +2,25 @@ using UnityEngine;
 
 public class Coin : MonoBehaviour
 {
+    [Header("이펙트 설정")]
+    public GameObject coinBurstEffect;
     private void OnTriggerEnter(Collider other)
     {
         // 충돌한 객체가 플레이어인지 확인 (대소문자 일치 필요)
         if (other.CompareTag("Player"))
         {
             // 플레이어가 코인을 먹었으므로 코인 오브젝트 파괴
-            Destroy(gameObject);
+            CollectCoin();
         }
+    }
+
+    private void CollectCoin()
+    {
+        if (coinBurstEffect != null)
+        {
+            Instantiate(coinBurstEffect, transform.position, transform.rotation);
+        }
+
+        Destroy(gameObject);
     }
 }
